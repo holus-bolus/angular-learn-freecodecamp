ab0348ef3a8a79b093c50e2ededa6271360f1b32
Oleksii <stinovoleksij@gmail.com>
2022-12-11 17:04:07 +0200

    Added rooms to the app

--- my-app/src/app/rooms/rooms.component.ts
+++ my-app/src/app/rooms/rooms.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {Room, RoomsList} from "./rooms";
 
 @Component({
   selector: 'app-rooms',
@@ -12,6 +13,37 @@ export class RoomsComponent implements OnInit {
   constructor() {
   }
 
+  hideRooms = false;
+
+  rooms: Room = {
+    totalRooms: 20,
+    availableRooms: 10,
+    bookedRooms: 5,
+  };
+
+  roomList: RoomsList [] = [
+    {roomNumber:1,
+      roomType: 'Deluxe room',
+      amenities: 'Air conditioner',
+      price: 500,
+      photos: 'https://luskinconferencecenter.ucla.edu/wp-content/uploads/2018/10/suite-image.jpg',
+      checkIntime: new Date('11-November-2021'),
+      checkoutTime: new Date('12-November-2021')
+    },
+    {roomNumber:2,
+      roomType: 'Deluxe room',
+      amenities: 'Air conditioner',
+      price: 500,
+      photos: 'https://www.caesars.com/content/dam/empire/enterprise-sales-promotions/caesar-suites/lvm/rio-super-suite.jpg.transform/card-img/image.jpg',
+      checkIntime: new Date('11-November-2021'),
+      checkoutTime: new Date('12-November-2021')
+    },
+  ]
+
   ngOnInit(): void {
+  }
+
+  toggle() {
+    this.hideRooms = !this.hideRooms;
   }
 }
---
+++ my-app/src/app/rooms/rooms.ts
@@ -0,0 +1,17 @@
+export interface Room {
+  totalRooms: number;
+  availableRooms: number;
+  bookedRooms: number;
+
+}
+
+
+export interface RoomsList {
+  roomNumber:number;
+  roomType: string;
+  amenities: string;
+  price: number;
+  photos: string;
+    checkIntime:Date;
+    checkoutTime:Date;
+}
